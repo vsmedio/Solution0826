82d0a9c16ec6b0eafafd4a0de3666616cafafd73
Arvis Abromovics <info@vsmedio.lv>
2018-01-08 16:51:17 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Пять победителей
+Создать массив на 20 чисел.
+Заполнить его числами с клавиатуры.
+Вывести пять наибольших чисел.
+Каждое значение с новой строки.
+
+
+Требования:
+1. Программа должна выводить числа на экран.
+2. Программа должна считывать значения с клавиатуры.
+3. Класс Solution должен содержать два метода.
+4. Метод sort() должен сортировать массив чисел от большего к меньшему.
+5. Метод main() должен вызывать метод sort().
+6. Программа должна выводить пять наибольших чисел массива. Каждое значение с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+
+    public static void sort(int[] array) {
+        //напишите тут ваш код
+        int a;
+        for (int i=0;i<array.length;i++)
+            for (int j=i;j<array.length;j++)
+                if (array[j]>array[i]) {
+                    a = array[i];
+                    array [i] = array [j];
+                    array [j] = a;
+                }
+    }
+}
